e43ff1b2b4d0253cd20d05d35d509a42b528473c
Gonz0x <gonzafigueroa6400@gmail.com>
2025-04-01 13:43:21 -0400

    punto 3, no se si esta bien

---
+++ tp2_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define N 5
+#define M 7
+int main(){
+srand(time(NULL));
+int i,j;
+int mt[N][M];
+int (*puntero)[M] = mt;
+
+for(i = 0;i<N; i++)
+{
+    for(j = 0;j<M; j++)
+    {
+        puntero[i][j]=1+rand()%100;
+        printf("%d", puntero[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
