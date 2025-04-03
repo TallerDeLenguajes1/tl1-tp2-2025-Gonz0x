ea1c4a31a8aa6a5f4ab2683081abbd6ce0634135
Gonz0x <gonzafigueroa6400@gmail.com>
2025-04-02 21:46:22 -0300

    correcion de errores y funciones terminadas.

--- tp2_4.c
+++ tp2_4.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-void listarPCs(struct compu pcs[], int cantidad);
-void mostrarMasVieja(struct compu pcs[], int cantidad);
-void mostrarMasVeloz(struct compu pcs[], int cantidad);
+#include <time.h>
 
 struct compu {
  int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
@@ -13,26 +9,35 @@ struct compu {
  char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
 }typedef computadora;
 
-int main(){
-    int cantidad = 5;
-    computadora pc;
-    computadora *pcs = (computadora *)malloc(cantidad * sizeof(computadora));
+void listarPCs(computadora pcs[], int cantidad);
+void mostrarMasVieja(computadora pcs[], int cantidad);
+void mostrarMasVeloz(computadora pcs[], int cantidad);
+
+
+int main(){    
     srand(time(NULL));
-    pc.velocidad = 1+rand()%3;
-    pc.anio = 2015+rand()%2024;
-    pc.cantidad_nucleos = 1+rand()%8;
+    int cantidad = 5;
+    computadora *pcs = (computadora *)malloc(cantidad * sizeof(computadora)); //asignacion dinamica de memoria
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
         "Pentium"};
-    computadora compu[5];
+    //computadora pcs[5]; //variable arreglo de 5 elementos para almacenar las caracteristicas de las 5 pcs, la hago utilizando asignacion dinamica de memoria
+
     for (int i = 0; i < 5; i++)
-    {
-        
-        pc.tipo_cpu = 
+    {    //valores aleatorios
+        pcs[i].velocidad = 1+rand()%3;
+        pcs[i].anio = 2015+rand()%2024;
+        pcs[i].cantidad_nucleos = 1+rand()%8;
+        int indice_aleatorio= rand() % 6;
+        pcs[i].tipo_cpu = tipos[indice_aleatorio]; //para cada pc generada el campo tipo_cpu
+        //de la estructura apunta a una cadena seleccionada aleatoriamente del arreglo tipos
     }
-            
+
+    listarPCs(pcs, cantidad);
+    mostrarMasVieja(pcs, cantidad); 
+    mostrarMasVeloz(pcs, cantidad);
 
     for (int i = 0; i < cantidad; i++)
-    {
+    { //liberacion de memoria
         free(pcs[i].tipo_cpu);
     }
     free(pcs);
@@ -41,18 +46,18 @@ int main(){
     return 0;
 }
 
-void listarPCs(struct compu pcs[], int cantidad){
+void listarPCs(computadora pcs[], int cantidad){
     for (int i = 0; i < cantidad; i++)
     {
         printf("\nPC:%d", i+1);
         printf("\nVelocidad: %d", pcs[i].velocidad);
         printf("\nAnio: %d", pcs[i].anio);
         printf("\nCantidad de nucleos: %d", pcs[i].cantidad_nucleos);
-        printf("\nProcesador: %d", pcs[i].tipo_cpu);
+        printf("\nProcesador: %s", pcs[i].tipo_cpu);
     }
     
 }
-void mostrarMasVieja(struct compu pcs[], int cantidad){
+void mostrarMasVieja(computadora pcs[], int cantidad){
     int menor = 0;
     printf("\nPC mas vieja:");
     for (int i = 0; i < cantidad; i++)
@@ -66,10 +71,10 @@ void mostrarMasVieja(struct compu pcs[], int cantidad){
     printf("\nVelocidad: %d", pcs[menor].velocidad);
     printf("\nAnio: %d", pcs[menor].anio);
     printf("\nCantidad de nucleos: %d", pcs[menor].cantidad_nucleos);
-    printf("\nProcesador: %d", pcs[menor].tipo_cpu);
+    printf("\nProcesador: %s", pcs[menor].tipo_cpu);
 }
 
-void mostrarMasVeloz(struct compu pcs[], int cantidad){
+void mostrarMasVeloz(computadora pcs[], int cantidad){
     for (int  i = 0; i < cantidad; i++)
     {
         if (pcs[i].velocidad == 3)
@@ -77,7 +82,7 @@ void mostrarMasVeloz(struct compu pcs[], int cantidad){
             printf("\nVelocidad: %d", pcs[i].velocidad);
             printf("\nAnio: %d", pcs[i].anio);
             printf("\nCantidad de nucleos: %d", pcs[i].cantidad_nucleos);
-            printf("\nProcesador: %d", pcs[i].tipo_cpu);
+            printf("\nProcesador: %s", pcs[i].tipo_cpu);
         }
         
     }
